15209e8d201f359448e014c1231df7cae7f243ba
Stella Lie <ask@stellalie.com>
2016-05-12 18:50:08 +1000

    Basic stuff done

---
+++ samples/src/main/java/slackbot/SlackBotSpeechletRequestStreamHandler.java
@@ -0,0 +1,38 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+package slackbot;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+
+public class SlackbotSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+
+    private static final Set<String> supportedApplicationIds;
+
+    static {
+        /*
+         * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
+         * Alexa Skill and put the relevant Application Ids in this Set.
+         */
+        supportedApplicationIds = new HashSet<String>();
+        // supportedApplicationIds.add("amzn1.echo-sdk-ams.app.[unique-value-here]");
+    }
+
+    public SlackbotSpeechletRequestStreamHandler() {
+        super(new slackbot.SlackbotSpeechlet(), supportedApplicationIds);
+    }
+
+    public SlackbotSpeechletRequestStreamHandler(Speechlet speechlet, Set<String> supportedApplicationIds) {
+        super(speechlet, supportedApplicationIds);
+    }
+}
---
+++ samples/src/main/java/slackbot/SlackbotSpeechlet.java
@@ -0,0 +1,157 @@
+package slackbot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.ui.OutputSpeech;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
+import com.amazon.speech.ui.SsmlOutputSpeech;
+import com.amazon.speech.ui.Reprompt;
+import com.amazon.speech.ui.SimpleCard;
+
+
+public class SlackbotSpeechlet implements Speechlet {
+
+    private static final Logger log = LoggerFactory.getLogger(SlackbotSpeechlet.class);
+
+    private static final String SESSION_STAGE = "stage";
+    private static final String SESSION_JOKE_ID = "jokeid";
+
+    private static final int THE_MESSAGE_STAGE = 1;
+    private static final int READY_FOR_DELIVERY = 2;
+
+    @Override
+    public void onSessionStarted(final SessionStartedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        // any initialization logic goes here
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        return handleCanYouSendSlackMessageIntent(session);
+    }
+
+    @Override
+    public SpeechletResponse onIntent(final IntentRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onIntent requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        Intent intent = request.getIntent();
+        String intentName = (intent != null) ? intent.getName() : null;
+
+        // Ready for delivery? Listen
+        if (session.getAttributes().containsKey(SESSION_STAGE)) {
+            if ((Integer) session.getAttribute(SESSION_STAGE) == READY_FOR_DELIVERY) {
+                return handleSetupTheMessageIntent(session);
+            }
+        }
+
+        if ("CanYouSendSlackMessageIntent".equals(intentName)) {
+            return handleCanYouSendSlackMessageIntent(session);
+        } else if ("AMAZON.StopIntent".equals(intentName)) {
+            PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+            outputSpeech.setText("As you wish. Bye bye");
+            return SpeechletResponse.newTellResponse(outputSpeech);
+        } else if ("AMAZON.CancelIntent".equals(intentName)) {
+            PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+            outputSpeech.setText("Whatever you want, mate. Goodbye");
+            return SpeechletResponse.newTellResponse(outputSpeech);
+        } else {
+            throw new SpeechletException("Invalid Intent");
+        }
+    }
+
+    @Override
+    public void onSessionEnded(final SessionEndedRequest request, final Session session)
+            throws SpeechletException {
+        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(),
+                session.getSessionId());
+
+        // any session cleanup logic would go here
+    }
+
+    /**
+     * Selects a joke randomly and starts it off by saying "Knock knock".
+     *
+     * @param session
+     *            the session object
+     * @return SpeechletResponse the speechlet response
+     */
+    private SpeechletResponse handleCanYouSendSlackMessageIntent(final Session session) {
+        // Reprompt speech will be triggered if the user doesn't respond.
+        String repromptText = "Can't hear you, mate. What is it again?";
+
+        // The stage variable tracks the phase of the dialogue.
+        // When this function completes, it will be on stage 1.
+        session.setAttribute(SESSION_STAGE, READY_FOR_DELIVERY);
+        String speechOutput = "Yeah, as you wish. What's the message to be slacked?";
+
+        // Create the Simple card content.
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Slack Bot");
+        card.setContent(speechOutput);
+
+        SpeechletResponse response = newAskResponse(speechOutput, false, repromptText, false);
+        response.setCard(card);
+        return response;
+    }
+
+    /**
+     * Responds to the user saying "Who's there".
+     *
+     * @param session
+     *            the session object
+     * @return SpeechletResponse the speechlet response
+     */
+    private SpeechletResponse handleSetupTheMessageIntent(final Session session) {
+        String speechOutput = "OK. Slack message sent now. Happy day!";
+
+        SimpleCard card = new SimpleCard();
+        card.setTitle("Slack Bot");
+        card.setContent(speechOutput);
+
+        SsmlOutputSpeech outputSpeech = new SsmlOutputSpeech();
+        outputSpeech.setSsml("<speak>" + speechOutput + "</speak>");
+        return SpeechletResponse.newTellResponse(outputSpeech, card);
+    }
+
+    private SpeechletResponse newAskResponse(String stringOutput, boolean isOutputSsml,
+            String repromptText, boolean isRepromptSsml) {
+        OutputSpeech outputSpeech, repromptOutputSpeech;
+        if (isOutputSsml) {
+            outputSpeech = new SsmlOutputSpeech();
+            ((SsmlOutputSpeech) outputSpeech).setSsml(stringOutput);
+        } else {
+            outputSpeech = new PlainTextOutputSpeech();
+            ((PlainTextOutputSpeech) outputSpeech).setText(stringOutput);
+        }
+
+        if (isRepromptSsml) {
+            repromptOutputSpeech = new SsmlOutputSpeech();
+            ((SsmlOutputSpeech) repromptOutputSpeech).setSsml(repromptText);
+        } else {
+            repromptOutputSpeech = new PlainTextOutputSpeech();
+            ((PlainTextOutputSpeech) repromptOutputSpeech).setText(repromptText);
+        }
+        Reprompt reprompt = new Reprompt();
+        reprompt.setOutputSpeech(repromptOutputSpeech);
+        return SpeechletResponse.newAskResponse(outputSpeech, reprompt);
+    }
+}
